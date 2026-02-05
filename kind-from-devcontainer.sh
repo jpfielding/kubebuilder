6ef76c79eef0e869c90e592620d5c111fba5305f
JP Fielding <jp.fielding@gmail.com>
2026-02-05 12:23:50 +0000

    create kind from dev container

---
+++ kind-from-devcontainer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Allows us to create a kind cluster that can be accessed via host.docker.internal
+
+NAME=${1}
+
+kind create cluster --name "${NAME}" --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: ClusterConfiguration
+    apiServer:
+      certSANs:
+        - "host.docker.internal"
+        - "127.0.0.1"
+        - "localhost"
+EOF
+
+kubectl config set-cluster kind-kind --server=https://host.docker.internal:$(kubectl config view -o jsonpath='{.clusters[?(@.name=="kind-kind")].cluster.server}' | cut -d: -f3)
